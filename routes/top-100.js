7e87fa18dd5f5c54fc2e3e2c5c3fbf7a577f6781
Jason Moore <jason@zivtech.com>
2018-12-12 22:43:26 -0500

    Fixing top 100 rank in table.

--- routes/top-100.js
+++ routes/top-100.js
@@ -8,13 +8,6 @@ router.get('/', function(req, res, next) {
       res.send(JSON.stringify({"status": 500, "error": error, "response": "Internal Server Error"})); 
     } else {
       res.send(JSON.stringify({"status": 200, "error": error, "response": results}));
-      const content = JSON.stringify({"status": 200, "error": error, "response": results});
-      fs.writeFile('./data/top-100.json', content, 'utf8', function (error) {
-        if (error) {
-          return console.log(error);
-        }
-        console.log("The top-100.json file was saved to the data directory!");
-      });
     }
   });
 });
